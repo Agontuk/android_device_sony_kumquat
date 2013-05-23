93276771bdc76d308f7272393e6506968450e55e
Bhargav Upperla <bhargavuln@codeaurora.org>
2013-05-23 12:50:15 -0700

    Configure dalvik heap parameters for low memory devices
    Reduces after boot memory footprint by about 5-8MB
    Note: This is for low memory based devices only (~512MB RAM
    or less)
    
    Change-Id: I4b4b9ae3efaff96551457e566bbb9bd4f0f23fd5

--- kumquat.mk
+++ kumquat.mk
@@ -64,7 +64,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=5m \
     dalvik.vm.heapgrowthlimit=48m \
     dalvik.vm.heapsize=128m \
-    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heaptargetutilization=0.25 \
     dalvik.vm.heapminfree=1m \
     dalvik.vm.heapmaxfree=2m
 
