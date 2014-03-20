116e332eefa534dfbe65fcb175c010a66407c6b7
95A31 <x95a31xreg@gmail.com>
2014-03-20 14:19:02 +0100

    Update kumquat.mk

--- kumquat.mk
+++ kumquat.mk
@@ -23,14 +23,13 @@ PRODUCT_COPY_FILES += \
     
 # Device specific hardware configuration script    
 	$(LOCAL_PATH)/config/dash.conf:system/etc/dash.conf \
-    $(LOCAL_PATH)/prebuilt/hw_config.sh:system/etc/hw_config.sh \
+    	$(LOCAL_PATH)/prebuilt/hw_config.sh:system/etc/hw_config.sh \
 	$(LOCAL_PATH)/config/cflashlib.cfg:system/etc/cflashlib.cfg \
 	$(LOCAL_PATH)/config/flashled_param_config.cfg:system/etc/flashled_param_config.cfg
 
 
 # Device specific bootlogo and charging animation
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/logo-480x854.rle:root/logo.rle
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/logo-480x854.rle:root/logo.rle
 $(call inherit-product, device/sony/kumquat/prebuilt/resources-480x854.mk)
 
 
@@ -43,8 +42,7 @@ PRODUCT_COPY_FILES += \
 
 
 # Device specific USB configuration script  
-PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/config/init.st-ericsson.usb.rc:root/init.st-ericsson.usb.rc
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/config/init.st-ericsson.usb.rc:root/init.st-ericsson.usb.rc
 
 
 # Device specific keylayouts and touchscreen configurations files
@@ -61,7 +59,7 @@ PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/config/sensor00_f11_sensor0.idc:system/usr/idc/sensor00_f11_sensor0.idc \
    $(LOCAL_PATH)/config/synaptics_rmi4_i2c.idc:system/usr/idc/synaptics_rmi4_i2c.idc
    
-# Device specific proprieties
+# Display specific proprieties
 # References: 
 # - http://developer.android.com/guide/practices/screens_support.html
 # - http://en.wikipedia.org/wiki/Pixel_density#Calculation_of_monitor_PPI
@@ -69,5 +67,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := mdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=280 \
-    ro.config.low_ram=true
+    ro.sf.lcd_density=280
+   
+# Device specific proprieties
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.low_ram=true
