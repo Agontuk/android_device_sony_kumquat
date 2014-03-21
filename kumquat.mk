91ebbcef4bee52d1b0f589b0a7a9b66af0681230
Andrewas <andrewas2009@gmail.com>
2014-03-21 13:53:31 +0200

    Update kumquat tree, fixed zygote value in system.prop and disabled deprecated screenshot technique

--- kumquat.mk
+++ kumquat.mk
@@ -18,11 +18,11 @@ DEVICE_PACKAGE_OVERLAYS += device/sony/kumquat/overlay
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/config/init.st-ericsson.device.rc:root/init.st-ericsson.device.rc \
 	$(LOCAL_PATH)/config/media_profiles.xml:system/etc/media_profiles.xml
-    
-    
+
+
 # Device specific hardware configuration script    
 	$(LOCAL_PATH)/config/dash.conf:system/etc/dash.conf \
-    	$(LOCAL_PATH)/prebuilt/hw_config.sh:system/etc/hw_config.sh \
+	$(LOCAL_PATH)/prebuilt/hw_config.sh:system/etc/hw_config.sh \
 	$(LOCAL_PATH)/config/cflashlib.cfg:system/etc/cflashlib.cfg \
 	$(LOCAL_PATH)/config/flashled_param_config.cfg:system/etc/flashled_param_config.cfg
 
@@ -36,24 +36,24 @@ $(call inherit-product, device/sony/kumquat/prebuilt/resources-480x854.mk)
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/config/bootrec-device:root/sbin/bootrec-device
 
-# Device specific USB configuration script  
+# Device specific USB configuration script
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/config/init.st-ericsson.usb.rc:root/init.st-ericsson.usb.rc
 
 
 # Device specific keylayouts and touchscreen configurations files
 PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/config/AB8500_Hs_Button.kl:system/usr/keylayout/AB8500_Hs_Button.kl \
-   $(LOCAL_PATH)/config/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
-   $(LOCAL_PATH)/config/simple_remote_appkey.kl:system/usr/keylayout/simple_remote_appkey.kl \
-   $(LOCAL_PATH)/config/cyttsp_key.kl:system/usr/keylayout/cyttsp_key.kl \
-   $(LOCAL_PATH)/config/STMPE-keypad.kl:system/usr/keylayout/STMPE-keypad.kl \
-   $(LOCAL_PATH)/config/tc3589x-keypad.kl:system/usr/keylayout/tc3589x-keypad.kl \
-   $(LOCAL_PATH)/config/ux500-ske-keypad.kl:system/usr/keylayout/ux500-ske-keypad.kl \
-   $(LOCAL_PATH)/config/ux500-ske-keypad-qwertz.kl:system/usr/keylayout/ux500-ske-keypad-qwertz.kl \
-   $(LOCAL_PATH)/config/cyttsp-spi.idc:system/usr/idc/cyttsp-spi.idc \
-   $(LOCAL_PATH)/config/sensor00_f11_sensor0.idc:system/usr/idc/sensor00_f11_sensor0.idc \
-   $(LOCAL_PATH)/config/synaptics_rmi4_i2c.idc:system/usr/idc/synaptics_rmi4_i2c.idc
-   
+	$(LOCAL_PATH)/config/AB8500_Hs_Button.kl:system/usr/keylayout/AB8500_Hs_Button.kl \
+	$(LOCAL_PATH)/config/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
+	$(LOCAL_PATH)/config/simple_remote_appkey.kl:system/usr/keylayout/simple_remote_appkey.kl \
+	$(LOCAL_PATH)/config/cyttsp_key.kl:system/usr/keylayout/cyttsp_key.kl \
+	$(LOCAL_PATH)/config/STMPE-keypad.kl:system/usr/keylayout/STMPE-keypad.kl \
+	$(LOCAL_PATH)/config/tc3589x-keypad.kl:system/usr/keylayout/tc3589x-keypad.kl \
+	$(LOCAL_PATH)/config/ux500-ske-keypad.kl:system/usr/keylayout/ux500-ske-keypad.kl \
+	$(LOCAL_PATH)/config/ux500-ske-keypad-qwertz.kl:system/usr/keylayout/ux500-ske-keypad-qwertz.kl \
+	$(LOCAL_PATH)/config/cyttsp-spi.idc:system/usr/idc/cyttsp-spi.idc \
+	$(LOCAL_PATH)/config/sensor00_f11_sensor0.idc:system/usr/idc/sensor00_f11_sensor0.idc \
+	$(LOCAL_PATH)/config/synaptics_rmi4_i2c.idc:system/usr/idc/synaptics_rmi4_i2c.idc
+
 # Device specific display resolutions
 # Reference: http://developer.android.com/guide/practices/screens_support.html
 # Note: In PRODUCT_AAPT_PREF_CONFIG set the proper one (e.g. hdpi), in PRODUCT_AAPT_CONFIG set the proper one and the previous one (e.g. mdpi)
