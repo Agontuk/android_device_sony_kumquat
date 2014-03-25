2b58de4bea9c56975d32723e1e5e21195763711e
95A31 <x95a31xreg@gmail.com>
2014-03-25 13:14:54 +0100

    Marrige <3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,8 +16,6 @@ TARGET_KERNEL_CONFIG := xperiau_defconfig
 # TWRP
 DEVICE_RESOLUTION := 480x854
 BOARD_HAS_NO_REAL_SDCARD := true
-
-# Specific recovery configuration
 BOARD_HAS_SDCARD_INTERNAL := true
 
 
