979a069db3ad5cecbb3d903ebde968ffdea929f1
DevSwift <devswift@devswift-R520-R522-R620.(none)>
2014-04-18 01:15:46 +0200

    up

---
+++ hardware/common.mk
@@ -0,0 +1,53 @@
+
+# modem_lib
+PRODUCT_PACKAGES += \
+   libisimessage \
+   libmalat \
+   libmalcs \
+   libmalftd \
+   libmalgpds \
+   libmalgss \
+   libmalmce \
+   libmalmis \
+   libmalmon \
+   libmalmte \
+   libmalnet \
+   libmalnvd \
+   libmalpipe \
+   libmalrf \
+   libmalsms \
+   libmaluicc \
+   libmalutil \
+   libshmnetlnk \
+   libphonet
+
+# cspsa
+PRODUCT_PACKAGES += \
+   cspsa-server \
+   libcspsa \
+   cspsa.conf \
+   cspsa-cmd \
+   libcspsa-core \
+   libcspsa-ll-file
+
+# cg2900
+PRODUCT_PACKAGES += \
+   ste-cg29xx_ctrl
+
+# psdata
+PRODUCT_PACKAGES += \
+   libstecom \
+   libmpl \
+   libpscc \
+   libsterc \
+   stercd
+
+PRODUCT_PACKAGES += \
+   chargemode \
+   dbus-daemon \
+   watchdog-kicker
+
+PRODUCT_PACKAGES += \
+   libdbus \
+   libarchive \
+   libarchive_fe
