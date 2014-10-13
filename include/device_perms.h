2e6f4b34de02bbdcac52b224188fa24a87ac409e
Agontuk <rifat662@gmail.com>
2014-10-13 23:48:29 +0600

    Fix camera property_perms error in dmesg

---
+++ include/device_perms.h
@@ -0,0 +1,8 @@
+#ifndef DEVICE_PERMS_H
+#define DEVICE_PERMS_H
+
+#define PROPERTY_PERMS_APPEND \
+    { "camera.0.",          AID_MEDIA,    0 }, \
+    { "camera.1.",          AID_MEDIA,    0 },
+
+#endif /* DEVICE_PERMS_H */
