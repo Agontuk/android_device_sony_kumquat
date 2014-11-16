17e3cbe5126afa5354a8978c3dd59d49bda1a2d9
Agontuk <rifat662@gmail.com>
2014-11-16 18:56:34 +0600

    Initial changes for CM

---
+++ cm.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
+$(call inherit-product, device/sony/kumquat/full_kumquat.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=ST25i_1262-1172 \
+    PRIVATE_BUILD_DESC="ST25i-user 4.0.4 6.1.1.B.1.100 m7t3zw test-keys" \
+    BUILD_FINGERPRINT="SEMC/ST25i_1262-1172/ST25i:4.0.4/6.1.1.B.1.100/m7t3zw:user/release-keys"
+
+# Device identifier
+PRODUCT_DEVICE := kumquat
+PRODUCT_NAME := cm_kumquat
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Xperia U
+PRODUCT_MANUFACTURER := Sony
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo full_kumquat-eng
-add_lunch_combo full_kumquat-userdebug
-add_lunch_combo full_kumquat-user
+add_lunch_combo cm_kumquat-eng
+add_lunch_combo cm_kumquat-userdebug
+add_lunch_combo cm_kumquat-user
